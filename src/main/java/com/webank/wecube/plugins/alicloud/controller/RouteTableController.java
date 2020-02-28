396a851b67511fcde708ac07268a0925c6cc251b
HoweChen <yuhao.howe.chen@gmail.com>
2020-02-28 09:35:42 +0800

    #9 update RouteTableController

--- src/main/java/com/webank/wecube/plugins/alicloud/controller/RouteTableController.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/controller/RouteTableController.java
@@ -1,5 +1,6 @@
 package com.webank.wecube.plugins.alicloud.controller;
 
+import com.aliyuncs.vpc.model.v20160428.AssociateRouteTableRequest;
 import com.aliyuncs.vpc.model.v20160428.DeleteRouteTableRequest;
 import com.webank.wecube.plugins.alicloud.common.ApplicationConstants;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
@@ -8,6 +9,7 @@ import com.webank.wecube.plugins.alicloud.dto.CoreResponseDto;
 import com.webank.wecube.plugins.alicloud.dto.routeTable.CoreCreateRouteTableRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.routeTable.CoreCreateRouteTableResponseDto;
 import com.webank.wecube.plugins.alicloud.service.routeTable.RouteTableService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +29,7 @@ public class RouteTableController {
         this.routeTableService = routeTableService;
     }
 
-    @PostMapping(path = "/")
+    @PostMapping(path = StringUtils.EMPTY)
     @ResponseBody
     public CoreResponseDto<?> createRouteTable(@RequestBody CoreRequestDto<CoreCreateRouteTableRequestDto> requestBody) {
         List<CoreCreateRouteTableResponseDto> result;
@@ -39,11 +41,22 @@ public class RouteTableController {
         return new CoreResponseDto<CoreCreateRouteTableResponseDto>().okayWithData(result);
     }
 
-    @DeleteMapping(path = "/")
+    @DeleteMapping(path = StringUtils.EMPTY)
     @ResponseBody
     public CoreResponseDto<?> deleteRouteTable(@RequestBody CoreRequestDto<DeleteRouteTableRequest> request) {
         try {
             this.routeTableService.deleteRouteTable(request.getInputs());
+        } catch (PluginException ex) {
+            return CoreResponseDto.error(ex.getMessage());
+        }
+        return CoreResponseDto.okay();
+    }
+
+    @PostMapping(path = "/vswitch")
+    @ResponseBody
+    public CoreResponseDto<?> associateVSwitch(@RequestBody CoreRequestDto<AssociateRouteTableRequest> request) {
+        try {
+            this.routeTableService.associateVSwitch(request.getInputs());
         } catch (PluginException ex) {
             return CoreResponseDto.error(ex.getMessage());
         }
