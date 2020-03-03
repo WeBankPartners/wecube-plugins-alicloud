9baa0c3b8d64e775de3e88d117bfe3bc2363a3d9
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-03 11:09:52 +0800

    #1 update VpcController

--- src/main/java/com/webank/wecube/plugins/alicloud/controller/VpcController.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/controller/VpcController.java
@@ -1,14 +1,13 @@
 package com.webank.wecube.plugins.alicloud.controller;
 
-import com.aliyuncs.vpc.model.v20160428.DeleteVpcRequest;
 import com.webank.wecube.plugins.alicloud.common.ApplicationConstants;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import com.webank.wecube.plugins.alicloud.dto.CoreRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.CoreResponseDto;
 import com.webank.wecube.plugins.alicloud.dto.vpc.CoreCreateVpcRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.vpc.CoreCreateVpcResponseDto;
+import com.webank.wecube.plugins.alicloud.dto.vpc.CoreDeleteVpcRequestDto;
 import com.webank.wecube.plugins.alicloud.service.vpc.VpcService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +23,7 @@ public class VpcController {
     @Autowired
     private VpcService vpcService;
 
-    @PostMapping(path = StringUtils.EMPTY)
+    @PostMapping(path = "/create")
     @ResponseBody
     public CoreResponseDto<?> createVpc(@RequestBody CoreRequestDto<CoreCreateVpcRequestDto> coreRequestDto) {
         List<CoreCreateVpcResponseDto> result;
@@ -36,9 +35,9 @@ public class VpcController {
         return new CoreResponseDto<CoreCreateVpcResponseDto>().okayWithData(result);
     }
 
-    @DeleteMapping(path = StringUtils.EMPTY)
+    @PostMapping(path = "/delete")
     @ResponseBody
-    public CoreResponseDto<?> deleteVpc(@RequestBody CoreRequestDto<DeleteVpcRequest> coreRequestDto) {
+    public CoreResponseDto<?> deleteVpc(@RequestBody CoreRequestDto<CoreDeleteVpcRequestDto> coreRequestDto) {
         try {
             this.vpcService.deleteVpc(coreRequestDto.getInputs());
         } catch (PluginException ex) {
