b7aa9cd92597bb9d275f11f2c9bfef25b18e8c71
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-06 14:35:01 +0800

    #16 update LoadBalancerController, add create and delete listener method

--- src/main/java/com/webank/wecube/plugins/alicloud/controller/LoadBalancerController.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/controller/LoadBalancerController.java
@@ -1,22 +1,27 @@
 package com.webank.wecube.plugins.alicloud.controller;
 
+import com.webank.wecube.plugins.alicloud.common.ApplicationConstants;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import com.webank.wecube.plugins.alicloud.dto.CoreRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.CoreResponseDto;
 import com.webank.wecube.plugins.alicloud.dto.loadBalancer.CoreCreateLoadBalancerRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.loadBalancer.CoreCreateLoadBalancerResponseDto;
 import com.webank.wecube.plugins.alicloud.dto.loadBalancer.CoreDeleteLoadBalancerRequestDto;
+import com.webank.wecube.plugins.alicloud.dto.loadBalancer.listener.CoreCreateLoadBalancerListenerRequestDto;
+import com.webank.wecube.plugins.alicloud.dto.loadBalancer.listener.CoreCreateLoadBalancerListenerResponseDto;
+import com.webank.wecube.plugins.alicloud.dto.loadBalancer.listener.CoreDeleteLoadBalancerListenerRequestDto;
+import com.webank.wecube.plugins.alicloud.dto.loadBalancer.listener.CoreDeleteLoadBalancerListenerResponseDto;
 import com.webank.wecube.plugins.alicloud.service.loadBalancer.LoadBalancerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * @author howechen
  */
+@RestController
+@RequestMapping(ApplicationConstants.ApiInfo.URL_PREFIX + "/load_balancer")
 public class LoadBalancerController {
     @Autowired
     private LoadBalancerService loadBalancerService;
@@ -42,5 +47,29 @@ public class LoadBalancerController {
             return CoreResponseDto.error(ex.getMessage());
         }
         return CoreResponseDto.okay();
+    }
+
+    @PostMapping(path = "/listener/create")
+    @ResponseBody
+    public CoreResponseDto<?> createListener(@RequestBody CoreRequestDto<CoreCreateLoadBalancerListenerRequestDto> request) {
+        List<CoreCreateLoadBalancerListenerResponseDto> result;
+        try {
+            result = this.loadBalancerService.createListener(request.getInputs());
+        } catch (PluginException ex) {
+            return CoreResponseDto.error(ex.getMessage());
+        }
+        return new CoreResponseDto<CoreCreateLoadBalancerListenerResponseDto>().okayWithData(result);
+    }
+
+    @PostMapping(path = "/listener/delete")
+    @ResponseBody
+    public CoreResponseDto<?> deleteListener(@RequestBody CoreRequestDto<CoreDeleteLoadBalancerListenerRequestDto> request) {
+        List<CoreDeleteLoadBalancerListenerResponseDto> result;
+        try {
+            result = this.loadBalancerService.deleteListener(request.getInputs());
+        } catch (PluginException ex) {
+            return CoreResponseDto.error(ex.getMessage());
+        }
+        return new CoreResponseDto<CoreDeleteLoadBalancerListenerResponseDto>().okayWithData(result);
     }
 }
