536f21d2dbaf078a2aa5c3709bb8c24397c8db82
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-10 15:55:49 +0800

    #19 update SecurityGroupController with security policy feature

--- src/main/java/com/webank/wecube/plugins/alicloud/controller/SecurityGroupController.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/controller/SecurityGroupController.java
@@ -4,9 +4,7 @@ import com.webank.wecube.plugins.alicloud.common.ApplicationConstants;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import com.webank.wecube.plugins.alicloud.dto.CoreRequestDto;
 import com.webank.wecube.plugins.alicloud.dto.CoreResponseDto;
-import com.webank.wecube.plugins.alicloud.dto.securityGroup.CoreCreateSecurityGroupRequestDto;
-import com.webank.wecube.plugins.alicloud.dto.securityGroup.CoreCreateSecurityGroupResponseDto;
-import com.webank.wecube.plugins.alicloud.dto.securityGroup.CoreDeleteSecurityGroupRequestDto;
+import com.webank.wecube.plugins.alicloud.dto.securityGroup.*;
 import com.webank.wecube.plugins.alicloud.service.securityGroup.SecurityGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -41,12 +39,36 @@ public class SecurityGroupController {
 
     @PostMapping(path = "/delete")
     @ResponseBody
-    public CoreResponseDto<?> deleteVSwitch(@RequestBody CoreRequestDto<CoreDeleteSecurityGroupRequestDto> request) {
+    public CoreResponseDto<?> deleteSecurityGroup(@RequestBody CoreRequestDto<CoreDeleteSecurityGroupRequestDto> request) {
         try {
             this.securityGroupService.deleteSecurityGroup(request.getInputs());
         } catch (PluginException ex) {
             return CoreResponseDto.error(ex.getMessage());
         }
         return CoreResponseDto.okay();
+    }
+
+    @PostMapping(path = "/authorize")
+    @ResponseBody
+    public CoreResponseDto<?> authorizeSecurityGroup(@RequestBody CoreRequestDto<CoreAuthorizeSecurityGroupRequestDto> request) {
+        List<CoreAuthorizeSecurityGroupResponseDto> result;
+        try {
+            result = this.securityGroupService.authorizeSecurityGroup(request.getInputs());
+        } catch (PluginException ex) {
+            return CoreResponseDto.error(ex.getMessage());
+        }
+        return new CoreResponseDto<CoreAuthorizeSecurityGroupResponseDto>().okayWithData(result);
+    }
+
+    @PostMapping(path = "/revoke")
+    @ResponseBody
+    public CoreResponseDto<?> revokeSecurityGroup(@RequestBody CoreRequestDto<CoreRevokeSecurityGroupRequestDto> request) {
+        List<CoreRevokeSecurityGroupResponseDto> result;
+        try {
+            result = this.securityGroupService.revokeSecurityGroup(request.getInputs());
+        } catch (PluginException ex) {
+            return CoreResponseDto.error(ex.getMessage());
+        }
+        return new CoreResponseDto<CoreRevokeSecurityGroupResponseDto>().okayWithData(result);
     }
 }
