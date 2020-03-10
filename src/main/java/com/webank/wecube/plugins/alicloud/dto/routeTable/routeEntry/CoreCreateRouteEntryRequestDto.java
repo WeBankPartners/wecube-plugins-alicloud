fa6f76efd2869fb33cdf94a3742d03425ad438d1
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-10 17:04:11 +0800

    #20 add route entry related DTOs

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/routeTable/routeEntry/CoreCreateRouteEntryRequestDto.java
@@ -0,0 +1,55 @@
+package com.webank.wecube.plugins.alicloud.dto.routeTable.routeEntry;
+
+import com.aliyuncs.vpc.model.v20160428.CreateRouteEntryRequest;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author howechen
+ */
+public class CoreCreateRouteEntryRequestDto extends CreateRouteEntryRequest {
+    private String identityParams;
+    private String cloudParams;
+    private String guid;
+    private String callbackParameter;
+
+    public CoreCreateRouteEntryRequestDto() {
+    }
+
+    public static CreateRouteEntryRequest toSdk(CoreCreateRouteEntryRequestDto requestDto) {
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.convertValue(requestDto, CreateRouteEntryRequest.class);
+    }
+
+    public String getIdentityParams() {
+        return identityParams;
+    }
+
+    public void setIdentityParams(String identityParams) {
+        this.identityParams = identityParams;
+    }
+
+    public String getCloudParams() {
+        return cloudParams;
+    }
+
+    public void setCloudParams(String cloudParams) {
+        this.cloudParams = cloudParams;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getCallbackParameter() {
+        return callbackParameter;
+    }
+
+    public void setCallbackParameter(String callbackParameter) {
+        this.callbackParameter = callbackParameter;
+    }
+}
