ab604eadd0fb36d6f3386cdc9329776c380a5c99
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-17 23:11:04 +0800

    #24 add CenController

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/controller/CenController.java
@@ -0,0 +1,54 @@
+package com.webank.wecube.plugins.alicloud.controller;
+
+import com.webank.wecube.plugins.alicloud.common.ApplicationConstants;
+import com.webank.wecube.plugins.alicloud.dto.CoreRequestDto;
+import com.webank.wecube.plugins.alicloud.dto.CoreResponseDto;
+import com.webank.wecube.plugins.alicloud.dto.cen.*;
+import com.webank.wecube.plugins.alicloud.service.cen.CenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author howechen
+ */
+@RestController
+@RequestMapping(ApplicationConstants.ApiInfo.URL_PREFIX + "/cen")
+public class CenController {
+
+    private CenService cenService;
+
+    @Autowired
+    public CenController(CenService cenService) {
+        this.cenService = cenService;
+    }
+
+    @PostMapping(path = "/create")
+    @ResponseBody
+    public CoreResponseDto<CoreCreateCenResponseDto> createCen(@RequestBody CoreRequestDto<CoreCreateCenRequestDto> request) {
+        List<CoreCreateCenResponseDto> result = this.cenService.createCen(request.getInputs());
+        return new CoreResponseDto<CoreCreateCenResponseDto>().withErrorCheck(result);
+    }
+
+    @PostMapping(path = "/delete")
+    @ResponseBody
+    public CoreResponseDto<CoreDeleteCenResponseDto> deleteCen(@RequestBody CoreRequestDto<CoreDeleteCenRequestDto> request) {
+        List<CoreDeleteCenResponseDto> result = this.cenService.deleteCen(request.getInputs());
+        return new CoreResponseDto<CoreDeleteCenResponseDto>().withErrorCheck(result);
+    }
+
+    @PostMapping(path = "/attach")
+    @ResponseBody
+    public CoreResponseDto<CoreAttachCenChildResponseDto> attachCenChild(@RequestBody CoreRequestDto<CoreAttachCenChildRequestDto> request) {
+        List<CoreAttachCenChildResponseDto> result = this.cenService.attachCenChild(request.getInputs());
+        return new CoreResponseDto<CoreAttachCenChildResponseDto>().withErrorCheck(result);
+    }
+
+    @PostMapping(path = "/detach")
+    @ResponseBody
+    public CoreResponseDto<CoreDetachCenChildResponseDto> detachCenChild(@RequestBody CoreRequestDto<CoreDetachCenChildRequestDto> request) {
+        List<CoreDetachCenChildResponseDto> result = this.cenService.detachCenChild(request.getInputs());
+        return new CoreResponseDto<CoreDetachCenChildResponseDto>().withErrorCheck(result);
+    }
+}
