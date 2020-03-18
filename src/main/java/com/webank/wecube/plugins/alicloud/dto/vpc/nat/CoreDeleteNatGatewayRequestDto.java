bafba4315b7270a3616b5c2cc5b7d6c5d9c1dae1
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-18 17:11:36 +0800

    #23 Add NAT gateway related DTOs

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/nat/CoreDeleteNatGatewayRequestDto.java
@@ -0,0 +1,72 @@
+package com.webank.wecube.plugins.alicloud.dto.vpc.nat;
+
+import com.aliyuncs.vpc.model.v20160428.DeleteNatGatewayRequest;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author howechen
+ */
+public class CoreDeleteNatGatewayRequestDto extends DeleteNatGatewayRequest {
+    @NotEmpty(message = "identityParams cannot be null or empty")
+    private String identityParams;
+    @NotEmpty(message = "cloudParams cannot be null or empty")
+    private String cloudParams;
+    private String guid = StringUtils.EMPTY;
+    private String callbackParameter = StringUtils.EMPTY;
+
+    @JsonDeserialize(as = Boolean.class)
+    @Override
+    public void setForce(Boolean force) {
+        super.setForce(force);
+    }
+
+    @JsonDeserialize(as = Long.class)
+    @Override
+    public void setResourceOwnerId(Long resourceOwnerId) {
+        super.setResourceOwnerId(resourceOwnerId);
+    }
+
+    @JsonDeserialize(as = Long.class)
+    @Override
+    public void setOwnerId(Long ownerId) {
+        super.setOwnerId(ownerId);
+    }
+
+    public CoreDeleteNatGatewayRequestDto() {
+    }
+
+    public String getIdentityParams() {
+        return identityParams;
+    }
+
+    public void setIdentityParams(String identityParams) {
+        this.identityParams = identityParams;
+    }
+
+    public String getCloudParams() {
+        return cloudParams;
+    }
+
+    public void setCloudParams(String cloudParams) {
+        this.cloudParams = cloudParams;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getCallbackParameter() {
+        return callbackParameter;
+    }
+
+    public void setCallbackParameter(String callbackParameter) {
+        this.callbackParameter = callbackParameter;
+    }
+}
