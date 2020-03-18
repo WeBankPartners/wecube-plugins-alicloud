e116170dc41b84659a1230d505046befa29294a4
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-18 16:37:20 +0800

    #4 update PluginStringUtils.java add another stringifyList to support list as input

--- src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginStringUtils.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginStringUtils.java
@@ -1,10 +1,23 @@
 package com.webank.wecube.plugins.alicloud.utils;
 
+import java.util.List;
 import java.util.StringJoiner;
 
+/**
+ * @author howechen
+ */
 public class PluginStringUtils {
 
     public static String stringifyList(String... strings) {
+        StringJoiner joiner = new StringJoiner(", ", "[", "]");
+
+        for (String string : strings) {
+            joiner.add("\"" + string + "\"");
+        }
+        return joiner.toString();
+    }
+
+    public static String stringifyList(List<String> strings) {
         StringJoiner joiner = new StringJoiner(", ", "[", "]");
 
         for (String string : strings) {
