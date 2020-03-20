0d018fc40faf134a088803e66f96061f69dee575
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-20 15:48:56 +0800

    #4 add PluginConstant with general constants

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/PluginConstant.java
@@ -0,0 +1,9 @@
+package com.webank.wecube.plugins.alicloud.support;
+
+/**
+ * @author howechen
+ */
+public class PluginConstant {
+    public static final int TIMEOUT_LIMIT = 15;
+    public static final int COUNT_DOWN_TIME = 15;
+}
