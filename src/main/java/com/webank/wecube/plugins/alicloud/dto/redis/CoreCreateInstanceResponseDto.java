76d851a03352b761e9d3bf8e092df8434719e17c
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-23 16:51:05 +0800

    #28 update redis response DTO

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/redis/CoreCreateInstanceResponseDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/redis/CoreCreateInstanceResponseDto.java
@@ -1,7 +1,8 @@
 package com.webank.wecube.plugins.alicloud.dto.redis;
 
 import com.aliyuncs.r_kvstore.model.v20150101.CreateInstanceResponse;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * @author howechen
@@ -10,31 +11,31 @@ public class CoreCreateInstanceResponseDto extends CreateInstanceResponse {
     private String guid;
     private String callbackParameter;
 
-    @JsonDeserialize(as = Integer.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     @Override
     public void setPort(Integer port) {
         super.setPort(port);
     }
 
-    @JsonDeserialize(as = Long.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     @Override
     public void setCapacity(Long capacity) {
         super.setCapacity(capacity);
     }
 
-    @JsonDeserialize(as = Long.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     @Override
     public void setQPS(Long qPS) {
         super.setQPS(qPS);
     }
 
-    @JsonDeserialize(as = Long.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     @Override
     public void setBandwidth(Long bandwidth) {
         super.setBandwidth(bandwidth);
     }
 
-    @JsonDeserialize(as = Long.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     @Override
     public void setConnections(Long connections) {
         super.setConnections(connections);
