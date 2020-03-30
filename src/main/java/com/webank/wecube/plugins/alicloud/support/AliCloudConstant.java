9883467c0a1e2ac40b3b4720208831253e82bff6
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-30 14:34:59 +0800

    #4 remove VM_INSTANTCE_STOP_STATUS

--- src/main/java/com/webank/wecube/plugins/alicloud/support/AliCloudConstant.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/AliCloudConstant.java
@@ -6,5 +6,4 @@ package com.webank.wecube.plugins.alicloud.support;
 public class AliCloudConstant {
     public static String ROUTE_TABLE_TYPE_SYSTEM = "System";
     public static String RESOURCE_AVAILABLE_STATUS = "Available";
-    public static String VM_INSTANCE_STOP_STATUS = "Stopped";
 }
