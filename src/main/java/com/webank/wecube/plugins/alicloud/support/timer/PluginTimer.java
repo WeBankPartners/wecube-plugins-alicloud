29904f49d13c4cf4631d3ef1289a267c7d6bd737
HoweChen <yuhao.howe.chen@gmail.com>
2020-03-31 11:41:12 +0800

    #4 update PluginTimer and PluginTimerTask with better logging

--- src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
@@ -1,6 +1,8 @@
 package com.webank.wecube.plugins.alicloud.support.timer;
 
 import com.webank.wecube.plugins.alicloud.common.PluginException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.*;
 
@@ -8,18 +10,30 @@ import java.util.concurrent.*;
  * @author howechen
  */
 public interface PluginTimer {
+
+    Logger logger = LoggerFactory.getLogger(PluginTimer.class);
     int TIMEOUT = 15;
 
     /**
      * Run task with default timeout time.
      *
-     * @param task
+     * @param task runnable task
      */
     static void runTask(Runnable task) {
         runTask(task, TIMEOUT);
     }
 
+    /**
+     * Run task with default timeout time.
+     *
+     * @param task    the runnable task.
+     * @param timeout the specified timeout.
+     * @throws PluginException when handling the runnable task.
+     */
     static void runTask(Runnable task, int timeout) throws PluginException {
+
+        logger.info("The plugin is running task with the timeout: [{}]", timeout);
+
         ExecutorService service = Executors.newSingleThreadExecutor();
         final Future<?> scheduledFuture = service.submit(task);
         try {
--- src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimerTask.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimerTask.java
@@ -1,6 +1,8 @@
 package com.webank.wecube.plugins.alicloud.support.timer;
 
 import com.webank.wecube.plugins.alicloud.common.PluginException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -9,6 +11,8 @@ import java.util.function.Function;
  * @author howechen
  */
 public class PluginTimerTask implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(PluginTimerTask.class);
 
     private int DELAY_TIME = 5;
     private Function<?, Boolean> func;
@@ -26,7 +30,10 @@ public class PluginTimerTask implements Runnable {
 
     @Override
     public void run() throws PluginException {
+        logger.info("Running plugin timer task.");
         while (!this.func.apply(null)) {
+            logger.info("Running plugin timer task.");
+
             try {
                 TimeUnit.SECONDS.sleep(this.DELAY_TIME);
             } catch (InterruptedException e) {
