46f56c0b351f080b0255b59387a5fae228c0c107
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-03 17:54:50 +0800

    #4 update Utils

--- src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginMapUtils.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginMapUtils.java
@@ -1,7 +1,8 @@
 package com.webank.wecube.plugins.alicloud.utils;
 
-import java.util.Arrays;
-import java.util.Map;
+import com.webank.wecube.plugins.alicloud.common.PluginException;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -13,5 +14,19 @@ public class PluginMapUtils {
         return Arrays.stream(paramString.split(";"))
                 .map(entry -> entry.split("="))
                 .collect(Collectors.toMap(entry -> entry[0], entry -> entry[1]));
+    }
+
+    public static <K, V> Map<K, V> zipToMap(List<K> keyList, List<V> valueList) throws PluginException {
+        Map<K, V> result = new HashMap<>();
+        Iterator<K> keyIterator = keyList.iterator();
+        Iterator<V> valueIterator = valueList.iterator();
+        while (keyIterator.hasNext() && valueIterator.hasNext()) {
+            result.put(keyIterator.next(), valueIterator.next());
+        }
+        if (keyIterator.hasNext() || valueIterator.hasNext()) {
+            throw new PluginException("The keyList and valueList size doesn't match.");
+        }
+
+        return result;
     }
 }
