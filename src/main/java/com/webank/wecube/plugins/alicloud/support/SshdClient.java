664cbbf212d9640d3decd0f03e9955381dd25a0b
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-07 17:59:20 +0800

    #4 add ScpClient and SshdClient

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/SshdClient.java
@@ -0,0 +1,68 @@
+package com.webank.wecube.plugins.alicloud.support;
+
+
+import com.webank.wecube.plugins.alicloud.common.PluginException;
+import com.webank.wecube.plugins.alicloud.support.ssh2.RemoteCommand;
+import com.webank.wecube.plugins.alicloud.support.ssh2.RemoteCommandExecutorConfig;
+import com.webank.wecube.plugins.alicloud.support.ssh2.impl.PooledRemoteCommandExecutor;
+import com.webank.wecube.plugins.alicloud.support.ssh2.impl.SimpleRemoteCommand;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+
+/**
+ * @author howechen
+ */
+@Component
+public class SshdClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(SshdClient.class);
+
+
+    public String run(String host, String user, String password, Integer port, String command)
+            throws PluginException {
+        RemoteCommandExecutorConfig config = new RemoteCommandExecutorConfig();
+        config.setRemoteHost(host);
+        config.setUser(user);
+        config.setPsword(password);
+        config.setPort(port);
+
+        RemoteCommand cmd = new SimpleRemoteCommand(command);
+        PooledRemoteCommandExecutor executor = new PooledRemoteCommandExecutor();
+        executor.init(config);
+
+        String result = executor.execute(cmd);
+
+        executor.destroy();
+
+        logger.info("result is: " + result);
+        return result;
+    }
+
+    public String runWithReturn(String host, String user, String password, Integer port, String command)
+            throws PluginException {
+        RemoteCommandExecutorConfig config = new RemoteCommandExecutorConfig();
+        config.setRemoteHost(host);
+        config.setUser(user);
+        config.setPsword(password);
+        config.setPort(port);
+
+        RemoteCommand cmd = new SimpleRemoteCommand(command);
+        PooledRemoteCommandExecutor executor = new PooledRemoteCommandExecutor();
+        executor.init(config);
+
+        String result = executor.execute(cmd);
+
+        executor.destroy();
+
+        logger.info("result is: " + result);
+        if (StringUtils.isEmpty(result)) {
+            throw new PluginException("return is empty, please check !");
+        } else {
+            return result;
+        }
+    }
+}
