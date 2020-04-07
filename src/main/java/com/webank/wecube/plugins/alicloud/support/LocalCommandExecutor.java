3a84d5f007da3e5e33427e59b3bba84580100ee1
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-07 17:59:04 +0800

    #4 update SystemCommandExecutor.java to LocalCommandExecutor, add run with return result function

--- src/main/java/com/webank/wecube/plugins/alicloud/support/LocalCommandExecutor.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/LocalCommandExecutor.java
@@ -6,20 +6,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author howechen
  */
 @Component
-public class SystemCommandExecutor {
+public class LocalCommandExecutor {
 
-    private static final Logger logger = LoggerFactory.getLogger(SystemCommandExecutor.class);
+    private static final Logger logger = LoggerFactory.getLogger(LocalCommandExecutor.class);
     private ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
     private PluginExecuteResultHandler resultHandler = new PluginExecuteResultHandler();
 
 
-    public SystemCommandExecutor() {
+    public LocalCommandExecutor() {
+    }
+
+    public String runWithReturn(String command) throws PluginException {
+        StringBuilder result = new StringBuilder();
+        Process pro;
+        Runtime runTime = Runtime.getRuntime();
+        if (runTime == null) {
+            throw new PluginException("Create runtime false!");
+        }
+        try {
+            pro = runTime.exec(command);
+            BufferedReader input = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+            PrintWriter output = new PrintWriter(new OutputStreamWriter(pro.getOutputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+            input.close();
+            output.close();
+            pro.destroy();
+        } catch (IOException e) {
+            logger.error("Execute '{}' command failed.{}", command, e);
+        }
+        logger.info("Execute '{}' command successful", command);
+        return result.toString();
     }
 
     public void run(String command) throws PluginException {
