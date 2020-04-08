362f87d2a2dc32e3ff380b46f41b2f07073c1d19
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-08 17:26:58 +0800

    #4 update plugin utils, update PluginObjectUtils and add PluginResourceUtils

--- src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginObjectUtils.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginObjectUtils.java
@@ -1,7 +1,11 @@
 package com.webank.wecube.plugins.alicloud.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webank.wecube.plugins.alicloud.common.PluginException;
+import com.webank.wecube.plugins.alicloud.dto.ecs.disk.UnformattedDiskDto;
+import net.bytebuddy.build.Plugin;
 
 /**
  * @author howechen
@@ -11,5 +15,14 @@ public class PluginObjectUtils {
     public static <T, V> T mapToObject(V requestDto, Class<T> clazz) {
         ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper.convertValue(requestDto, clazz);
+    }
+
+    public static <T, V> T mapJsonStringToObject(String jsonString, Class<T> clazz) throws PluginException {
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            return mapper.readValue(jsonString, clazz);
+        } catch (JsonProcessingException e) {
+            throw new PluginException("Error while transforming JSON string to class");
+        }
     }
 }
