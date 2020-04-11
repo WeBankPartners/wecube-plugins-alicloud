8b6754c7843cb0e8a35db1b45b8d28a678aa12d5
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-12 01:10:44 +0800

    #4 update PluginTimer to support PluginTimerScheduleTask, also add InterruptTaskException to handle when the task has finished.

--- src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
@@ -4,6 +4,7 @@ import com.webank.wecube.plugins.alicloud.common.PluginException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Date;
 import java.util.concurrent.*;
 
 /**
@@ -42,6 +43,45 @@ public interface PluginTimer {
             throw new PluginException(String.format("The running task thread has been interrupted. The error is: %s", e.getMessage()));
         } catch (ExecutionException e) {
             throw new PluginException(String.format("Encounter error while running the timer task. The error is: %s", e.getMessage()));
+        } catch (TimeoutException e) {
+            throw new PluginException("The request is timeout.");
+        } finally {
+            service.shutdownNow();
+        }
+    }
+
+    /**
+     * Run schedule task with default timeout time.
+     *
+     * @param task runnable task
+     */
+    static void runScheduleTask(PluginTimerScheduleTask task) {
+        runScheduleTask(task, TIMEOUT);
+    }
+
+    /**
+     * Run schedule task with default timeout time.
+     *
+     * @param task    the runnable task.
+     * @param timeout the specified timeout.
+     * @throws PluginException when handling the runnable task.
+     */
+    static void runScheduleTask(PluginTimerScheduleTask task, int timeout) throws PluginException {
+
+        logger.info("The plugin is running task with the timeout: [{}]", timeout);
+
+        final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+        final Future<?> scheduledFuture = service.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        try {
+            scheduledFuture.get(timeout, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new PluginException(String.format("The running task thread has been interrupted. The error is: %s", e.getMessage()));
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof InterruptTaskException) {
+                logger.info("Plugin timer task completed.");
+            } else {
+                throw new PluginException(String.format("Encounter error while running the timer task. The error is: %s", e.getMessage()));
+            }
         } catch (TimeoutException e) {
             throw new PluginException("The request is timeout.");
         } finally {
