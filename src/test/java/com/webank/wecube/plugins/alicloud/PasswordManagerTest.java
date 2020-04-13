6c466c71ea1401e4ac604d0ae10835567606e40e
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-13 16:44:14 +0800

    #32 update PasswordManagerTest

--- src/test/java/com/webank/wecube/plugins/alicloud/PasswordManagerTest.java
+++ src/test/java/com/webank/wecube/plugins/alicloud/PasswordManagerTest.java
@@ -9,10 +9,10 @@ public class PasswordManagerTest extends BaseSpringBootTest {
     @Autowired
     private PasswordManager passwordManager;
 
-    private String guid = "0015_0000000020";
-    private String seed = "seed-wecube2.1-2020";
-    private ***REMOVED***
-    private String encryptedPassword = "{cipher_a}5f5fb6fd46cd7cf9183893425d1d6506";
+    private final String guid = "0015_0000000020";
+    private final String seed = "seed-wecube2.1-2020";
+    private final ***REMOVED***
+    private final String encryptedPassword = "{cipher_a}5f5fb6fd46cd7cf9183893425d1d6506";
 
 
     @Test
