373f1516ac7e80882433fd3ed80abbb2f5548426
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-13 17:22:20 +0800

    #4 set up IntegrationTest

---
+++ src/test/java/com/webank/wecube/plugins/alicloud/Integration/IntegrationTest.java
@@ -0,0 +1,7 @@
+package com.webank.wecube.plugins.alicloud.Integration;
+
+import com.webank.wecube.plugins.alicloud.BaseSpringBootTest;
+import org.junit.Test;
+
+public class IntegrationTest extends BaseSpringBootTest {
+}
