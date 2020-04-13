e1bf639363c633c4ef78571f69f1d38a6a9baadd
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-13 16:33:03 +0800

    #4 remove PluginTimerScheduleTask.java and its call in PluginTimer due to the poor design.

--- src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/timer/PluginTimer.java
@@ -52,46 +52,4 @@ public interface PluginTimer {
             service.shutdownNow();
         }
     }
-
-    /**
-     * Run schedule task with default timeout time.
-     *
-     * @param task runnable task
-     */
-    static void runScheduleTask(PluginTimerScheduleTask task) {
-        runScheduleTask(task, TIMEOUT);
-    }
-
-    /**
-     * Run schedule task with default timeout time.
-     *
-     * @param task    the runnable task.
-     * @param timeout the specified timeout.
-     * @throws PluginException when handling the runnable task.
-     */
-    static void runScheduleTask(PluginTimerScheduleTask task, int timeout) throws PluginException {
-
-        logger.info("The plugin is running task with the timeout: [{}]", timeout);
-
-        final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        final Future<?> scheduledFuture = service.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
-        try {
-            scheduledFuture.get(timeout, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            throw new PluginException(String.format("The running task thread has been interrupted. The error is: %s", e.getMessage()));
-        } catch (ExecutionException e) {
-            if (e.getCause() instanceof InterruptTaskException) {
-                logger.info("Plugin timer task completed.");
-            } else {
-                throw new PluginException(String.format("Encounter error while running the timer task. The error is: %s", e.getMessage()));
-            }
-        } catch (TimeoutException e) {
-            throw new PluginException("The request is timeout.");
-        } finally {
-            if (!scheduledFuture.isCancelled()) {
-                scheduledFuture.cancel(true);
-            }
-            service.shutdownNow();
-        }
-    }
 }
