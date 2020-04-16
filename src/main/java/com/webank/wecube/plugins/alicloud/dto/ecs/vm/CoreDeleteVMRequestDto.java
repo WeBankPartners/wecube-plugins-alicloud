c7910dc1c37f6a6d0aed59d8c77582fd8c1fd344
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-16 11:47:46 +0800

    #10 #41 #33 update VM related DTOs

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/vm/CoreDeleteVMRequestDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/vm/CoreDeleteVMRequestDto.java
@@ -3,6 +3,9 @@ package com.webank.wecube.plugins.alicloud.dto.ecs.vm;
 import com.aliyuncs.ecs.model.v20140526.DeleteInstanceRequest;
 import com.webank.wecube.plugins.alicloud.dto.CoreRequestInputDto;
 import com.webank.wecube.plugins.alicloud.dto.PluginSdkInputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.validation.constraints.NotEmpty;
 
 /**
  * @author howechen
@@ -13,6 +16,7 @@ public class CoreDeleteVMRequestDto extends CoreRequestInputDto implements Plugi
     private String resourceOwnerAccount;
     private String ownerAccount;
     private String ownerId;
+    @NotEmpty(message = "instanceId field is mandatory.")
     private String instanceId;
     private String force;
 
@@ -73,5 +77,19 @@ public class CoreDeleteVMRequestDto extends CoreRequestInputDto implements Plugi
 
     public void setForce(String force) {
         this.force = force;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("resourceOwnerId", resourceOwnerId)
+                .append("terminateSubscription", terminateSubscription)
+                .append("resourceOwnerAccount", resourceOwnerAccount)
+                .append("ownerAccount", ownerAccount)
+                .append("ownerId", ownerId)
+                .append("instanceId", instanceId)
+                .append("force", force)
+                .toString();
     }
 }
--- src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/vm/CoreStartVMResponseDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/vm/CoreStartVMResponseDto.java
@@ -3,6 +3,7 @@ package com.webank.wecube.plugins.alicloud.dto.ecs.vm;
 import com.aliyuncs.ecs.model.v20140526.StartInstanceResponse;
 import com.webank.wecube.plugins.alicloud.dto.CoreResponseOutputDto;
 import com.webank.wecube.plugins.alicloud.dto.PluginSdkOutputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * @author howechen
@@ -19,5 +20,13 @@ public class CoreStartVMResponseDto extends CoreResponseOutputDto implements Plu
 
     public void setRequestId(String requestId) {
         this.requestId = requestId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("requestId", requestId)
+                .toString();
     }
 }
