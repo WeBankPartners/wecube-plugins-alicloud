1eabb8fc4ec6ad48b7b4cfd825b9b168461ddd1d
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-17 11:40:44 +0800

    #46 add PluginStringUtils to split coreAndMemoryString

--- src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginStringUtils.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginStringUtils.java
@@ -1,10 +1,15 @@
 package com.webank.wecube.plugins.alicloud.utils;
 
+import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author howechen
@@ -44,5 +49,22 @@ public class PluginStringUtils {
         str = StringUtils.removeEnd(str, "]");
 
         return Arrays.asList(str.split(","));
+    }
+
+    public static Pair<String, String> splitCoreAndMemory(String coreAndMemoryString) {
+
+        final String regex = "^(\\d*)[C|c](\\d*)[G|g]$";
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(coreAndMemoryString);
+        String core = StringUtils.EMPTY;
+        String memory = StringUtils.EMPTY;
+        // matcher group index: 0 - full group, 1 - core, 2 - memory
+        while (matcher.find()) {
+            core = matcher.group(1);
+            memory = matcher.group(2);
+        }
+
+        return new ImmutablePair<>(core, memory);
     }
 }
