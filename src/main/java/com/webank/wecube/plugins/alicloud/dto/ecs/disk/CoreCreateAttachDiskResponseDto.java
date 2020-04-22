ba54cc479949216197084bcb08ebbd11fcdf1c9f
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-22 17:26:20 +0800

    #49 update disk related DTOs

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/disk/CoreCreateAttachDiskResponseDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/ecs/disk/CoreCreateAttachDiskResponseDto.java
@@ -3,6 +3,7 @@ package com.webank.wecube.plugins.alicloud.dto.ecs.disk;
 import com.aliyuncs.ecs.model.v20140526.CreateDiskResponse;
 import com.webank.wecube.plugins.alicloud.dto.CoreResponseOutputDto;
 import com.webank.wecube.plugins.alicloud.dto.PluginSdkOutputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * @author howechen
@@ -37,5 +38,15 @@ public class CoreCreateAttachDiskResponseDto extends CoreResponseOutputDto imple
 
     public void setVolumeName(String volumeName) {
         this.volumeName = volumeName;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("requestId", requestId)
+                .append("diskId", diskId)
+                .append("volumeName", volumeName)
+                .toString();
     }
 }
