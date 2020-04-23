9e5efde3dc0ccfd7d3fa2edfcda8eb709ca2c440
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-23 11:12:15 +0800

    #52 update route entry related DTOs

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/routeTable/routeEntry/CoreDeleteRouteEntryRequestDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/routeTable/routeEntry/CoreDeleteRouteEntryRequestDto.java
@@ -3,7 +3,9 @@ package com.webank.wecube.plugins.alicloud.dto.vpc.routeTable.routeEntry;
 import com.aliyuncs.vpc.model.v20160428.DeleteRouteEntryRequest;
 import com.webank.wecube.plugins.alicloud.dto.CoreRequestInputDto;
 import com.webank.wecube.plugins.alicloud.dto.PluginSdkInputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 /**
@@ -12,9 +14,12 @@ import java.util.List;
 public class CoreDeleteRouteEntryRequestDto extends CoreRequestInputDto implements PluginSdkInputBridge<DeleteRouteEntryRequest> {
 
     private String resourceOwnerId;
+    @NotEmpty(message = "nextHopId field is mandatory")
     private String nextHopId;
+    @NotEmpty(message = "routeTableId field is mandatory")
     private String routeTableId;
     private String resourceOwnerAccount;
+    @NotEmpty(message = "destinationCidrBlock field is mandatory")
     private String destinationCidrBlock;
     private String ownerAccount;
     private String ownerId;
@@ -95,5 +100,21 @@ public class CoreDeleteRouteEntryRequestDto extends CoreRequestInputDto implemen
 
     public void setNextHopLists(List<DeleteRouteEntryRequest.NextHopList> nextHopLists) {
         this.nextHopLists = nextHopLists;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("resourceOwnerId", resourceOwnerId)
+                .append("nextHopId", nextHopId)
+                .append("routeTableId", routeTableId)
+                .append("resourceOwnerAccount", resourceOwnerAccount)
+                .append("destinationCidrBlock", destinationCidrBlock)
+                .append("ownerAccount", ownerAccount)
+                .append("ownerId", ownerId)
+                .append("routeEntryId", routeEntryId)
+                .append("nextHopLists", nextHopLists)
+                .toString();
     }
 }
