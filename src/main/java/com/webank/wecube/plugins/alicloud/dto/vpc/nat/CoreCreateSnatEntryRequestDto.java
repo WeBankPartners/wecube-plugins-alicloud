2eab7f33d06429a74f548e6f8d7674fc62194986
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-24 17:07:14 +0800

    #60 update SNAT related DTOs

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/nat/CoreCreateSnatEntryRequestDto.java
@@ -0,0 +1,137 @@
+package com.webank.wecube.plugins.alicloud.dto.vpc.nat;
+
+import com.aliyuncs.vpc.model.v20160428.CreateSnatEntryRequest;
+import com.webank.wecube.plugins.alicloud.dto.CoreRequestInputDto;
+import com.webank.wecube.plugins.alicloud.dto.PluginSdkInputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author howechen
+ */
+public class CoreCreateSnatEntryRequestDto extends CoreRequestInputDto implements PluginSdkInputBridge<CreateSnatEntryRequest> {
+
+    private String resourceOwnerId;
+
+    private String clientToken;
+
+    private String sourceCIDR;
+
+    @NotEmpty(message = "snatIp field is mandatory")
+    private String snatIp;
+
+    private String sourceVSwitchId;
+
+    private String resourceOwnerAccount;
+
+    private String ownerAccount;
+
+    @NotEmpty(message = "snatTableId field is mandatory")
+    private String snatTableId;
+
+    private String ownerId;
+
+    private String snatEntryName;
+
+
+    public CoreCreateSnatEntryRequestDto() {
+    }
+
+    public String getResourceOwnerId() {
+        return resourceOwnerId;
+    }
+
+    public void setResourceOwnerId(String resourceOwnerId) {
+        this.resourceOwnerId = resourceOwnerId;
+    }
+
+    public String getClientToken() {
+        return clientToken;
+    }
+
+    public void setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+    }
+
+    public String getSourceCIDR() {
+        return sourceCIDR;
+    }
+
+    public void setSourceCIDR(String sourceCIDR) {
+        this.sourceCIDR = sourceCIDR;
+    }
+
+    public String getSnatIp() {
+        return snatIp;
+    }
+
+    public void setSnatIp(String snatIp) {
+        this.snatIp = snatIp;
+    }
+
+    public String getSourceVSwitchId() {
+        return sourceVSwitchId;
+    }
+
+    public void setSourceVSwitchId(String sourceVSwitchId) {
+        this.sourceVSwitchId = sourceVSwitchId;
+    }
+
+    public String getResourceOwnerAccount() {
+        return resourceOwnerAccount;
+    }
+
+    public void setResourceOwnerAccount(String resourceOwnerAccount) {
+        this.resourceOwnerAccount = resourceOwnerAccount;
+    }
+
+    public String getOwnerAccount() {
+        return ownerAccount;
+    }
+
+    public void setOwnerAccount(String ownerAccount) {
+        this.ownerAccount = ownerAccount;
+    }
+
+    public String getSnatTableId() {
+        return snatTableId;
+    }
+
+    public void setSnatTableId(String snatTableId) {
+        this.snatTableId = snatTableId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getSnatEntryName() {
+        return snatEntryName;
+    }
+
+    public void setSnatEntryName(String snatEntryName) {
+        this.snatEntryName = snatEntryName;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("resourceOwnerId", resourceOwnerId)
+                .append("clientToken", clientToken)
+                .append("sourceCIDR", sourceCIDR)
+                .append("snatIp", snatIp)
+                .append("sourceVSwitchId", sourceVSwitchId)
+                .append("resourceOwnerAccount", resourceOwnerAccount)
+                .append("ownerAccount", ownerAccount)
+                .append("snatTableId", snatTableId)
+                .append("ownerId", ownerId)
+                .append("snatEntryName", snatEntryName)
+                .toString();
+    }
+}
---
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/nat/CoreDeleteSnatEntryResponseDto.java
@@ -0,0 +1,33 @@
+package com.webank.wecube.plugins.alicloud.dto.vpc.nat;
+
+import com.aliyuncs.vpc.model.v20160428.DeleteSnatEntryResponse;
+import com.webank.wecube.plugins.alicloud.dto.CoreResponseOutputDto;
+import com.webank.wecube.plugins.alicloud.dto.PluginSdkOutputBridge;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * @author howechen
+ */
+public class CoreDeleteSnatEntryResponseDto extends CoreResponseOutputDto implements PluginSdkOutputBridge<CoreDeleteSnatEntryResponseDto, DeleteSnatEntryResponse> {
+
+    private String requestId;
+
+    public CoreDeleteSnatEntryResponseDto() {
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("requestId", requestId)
+                .toString();
+    }
+}
