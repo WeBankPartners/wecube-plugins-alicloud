341113f32888121e50f5c3071e47056c23e42e04
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-24 10:40:08 +0800

    #4 update IntegrationTest

--- src/test/java/com/webank/wecube/plugins/alicloud/integration/IntegrationTest.java
+++ src/test/java/com/webank/wecube/plugins/alicloud/integration/IntegrationTest.java
@@ -1,7 +1,11 @@
-package com.webank.wecube.plugins.alicloud.Integration;
+package com.webank.wecube.plugins.alicloud.integration;
 
 import com.webank.wecube.plugins.alicloud.BaseSpringBootTest;
 import org.junit.Test;
 
 public class IntegrationTest extends BaseSpringBootTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("test");
+    }
 }
