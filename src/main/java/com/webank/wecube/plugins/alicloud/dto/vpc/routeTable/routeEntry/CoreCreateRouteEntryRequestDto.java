22ca24492731be49e1d8e410d298b82bb28e1d65
HoweChen <yuhao.howe.chen@gmail.com>
2020-04-24 23:40:26 +0800

    #52 update register.xml.tpl and CoreCreateRouteEntryRequestDto to make nextHopType mandatory

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/routeTable/routeEntry/CoreCreateRouteEntryRequestDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/vpc/routeTable/routeEntry/CoreCreateRouteEntryRequestDto.java
@@ -17,6 +17,7 @@ public class CoreCreateRouteEntryRequestDto extends CoreRequestInputDto implemen
     private String clientToken;
     @NotEmpty(message = "nextHopId field is mandatory")
     private String nextHopId;
+    @NotEmpty(message = "nextHopType filed is mandatory")
     private String nextHopType;
     @NotEmpty(message = "routeTableId field is mandatory")
     private String routeTableId;
