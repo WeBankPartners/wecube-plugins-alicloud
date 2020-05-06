a59a48f6005c50df6f1cbf4f7ae0a2857430b135
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-06 16:41:46 +0800

    #111 add RDSCategory enum class

---
+++ src/main/java/com/webank/wecube/plugins/alicloud/service/rds/RDSCategory.java
@@ -0,0 +1,15 @@
+package com.webank.wecube.plugins.alicloud.service.rds;
+
+/**
+ * @author howechen
+ */
+public enum RDSCategory {
+    // basic
+    Basic,
+    // high availability
+    HighAvailability,
+    // always on
+    AlwaysOn,
+    // finance
+    Finance
+}
