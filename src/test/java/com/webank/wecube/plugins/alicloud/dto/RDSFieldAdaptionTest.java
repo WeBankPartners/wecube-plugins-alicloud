153de4de525341d2e8269780a36774f548b0cac3
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-06 21:07:10 +0800

    #117 update RDSFieldAdaptionTest

--- src/test/java/com/webank/wecube/plugins/alicloud/dto/RDSFieldAdaptionTest.java
+++ src/test/java/com/webank/wecube/plugins/alicloud/dto/RDSFieldAdaptionTest.java
@@ -9,7 +9,7 @@ public class RDSFieldAdaptionTest {
     private final CoreCreateDBInstanceRequestDto requestDto = new CoreCreateDBInstanceRequestDto();
 
     @Test
-    void givenBasicCategory_handleZoneId_ShouldSucceed() {
+    void givenBasicCategory_handleZoneId_ShouldAdaptSucceed() {
         requestDto.setZoneId("ap-southeast-1maz1-b");
         requestDto.setCategory(RDSCategory.Basic.toString());
         requestDto.adaptToAliCloud();
@@ -17,10 +17,28 @@ public class RDSFieldAdaptionTest {
     }
 
     @Test
-    void givenOtherCategoryAndListStringZoneId_adaptToAliCloud_ShouldSucceed() {
+    void givenBasicCategoryAndValidZoneId_adaptToAliCloud_ShouldAdaptSucceed() {
+        requestDto.setCategory(RDSCategory.Basic.toString());
+        requestDto.setZoneId("ap-southeast-1a");
+        requestDto.adaptToAliCloud();
+        assert StringUtils.equals("ap-southeast-1a", requestDto.getZoneId());
+    }
+
+    @Test
+    void givenOtherCategoryAndListStringZoneId_adaptToAliCloud_ShouldAdaptSucceed() {
         requestDto.setCategory(RDSCategory.HighAvailability.toString());
         requestDto.setZoneId("[ap-southeast-1MAZ2-a, ap-southeast-1MAZ2-b]");
         requestDto.adaptToAliCloud();
         assert StringUtils.equals("ap-southeast-1MAZ2(a,b)", requestDto.getZoneId());
     }
+
+    @Test
+    void givenOtherCategoryAndValidStringZoneId_adaptToAliCloud_ShouldAdaptSucceed() {
+        requestDto.setCategory(RDSCategory.HighAvailability.toString());
+        requestDto.setZoneId("ap-southeast-1MAZ2(a,b)");
+        requestDto.adaptToAliCloud();
+        assert StringUtils.equals("ap-southeast-1MAZ2(a,b)", requestDto.getZoneId());
+    }
+
+
 }
