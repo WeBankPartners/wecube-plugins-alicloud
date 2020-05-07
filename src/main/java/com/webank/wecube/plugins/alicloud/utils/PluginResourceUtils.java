075dc14ec9691be1109177d7f238e977f944ec4e
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-07 20:22:52 +0800

    #129 update PluginResourceUtils to support Resource class

--- src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginResourceUtils.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/utils/PluginResourceUtils.java
@@ -3,11 +3,12 @@ package com.webank.wecube.plugins.alicloud.utils;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
 import org.springframework.util.ResourceUtils;
 
-import javax.annotation.Resource;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,6 +28,7 @@ public interface PluginResourceUtils {
      * @return file's absolute path
      * @throws PluginException when file cannot be found
      */
+    @Deprecated
     static String getResourceAbsolutePath(String resourceClassPath) throws PluginException {
         try {
             return ResourceUtils.getFile(resourceClassPath).getAbsolutePath();
@@ -42,6 +44,7 @@ public interface PluginResourceUtils {
      * @return file's absolute path
      * @throws PluginException when file cannot be found
      */
+    @Deprecated
     static List<String> getResourceAbsolutePath(String... resourceClassPaths) throws PluginException {
         List<String> resultList = new ArrayList<>();
         for (String resourceClassPath : resourceClassPaths) {
@@ -62,11 +65,44 @@ public interface PluginResourceUtils {
      * @return files' absolute path
      * @throws PluginException when file cannot be found
      */
+    @Deprecated
     static List<String> getAllResourceAbsolutePath(String directoryClassPath) throws PluginException {
         try {
             return Arrays.stream(Objects.requireNonNull(ResourceUtils.getFile(directoryClassPath).listFiles())).map(File::getAbsolutePath).collect(Collectors.toList());
         } catch (FileNotFoundException e) {
             throw new PluginException(e.getMessage());
         }
+    }
+
+    /**
+     * Get absolute path list from Resource array
+     *
+     * @param resources Resource array
+     * @return absolute path list
+     * @throws PluginException while getting the file
+     */
+    static List<String> getResourceAbsolutePath(org.springframework.core.io.Resource... resources) throws PluginException {
+        List<String> result = new ArrayList<>();
+        for (org.springframework.core.io.Resource resource : resources) {
+            result.add(getResourceAbsolutePath(resource));
+        }
+        return result;
+    }
+
+    /**
+     * Get absolute path from Resource
+     *
+     * @param resource Resource
+     * @return absolute path
+     * @throws PluginException while getting the file
+     */
+    static String getResourceAbsolutePath(Resource resource) throws PluginException {
+        String result;
+        try {
+            result = resource.getFile().getAbsolutePath();
+        } catch (IOException e) {
+            throw new PluginException(e.getMessage());
+        }
+        return result;
     }
 }
