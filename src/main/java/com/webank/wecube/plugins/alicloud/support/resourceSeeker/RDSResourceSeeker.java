79c26966daab301a877dcaae8ad613b06f935d11
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-11 16:03:28 +0800

    #125 update RDSResourceSeeker

--- src/main/java/com/webank/wecube/plugins/alicloud/support/resourceSeeker/RDSResourceSeeker.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/support/resourceSeeker/RDSResourceSeeker.java
@@ -3,13 +3,13 @@ package com.webank.wecube.plugins.alicloud.support.resourceSeeker;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.rds.model.v20140815.DescribeAvailableResourceRequest;
 import com.aliyuncs.rds.model.v20140815.DescribeAvailableResourceResponse;
-import com.google.common.collect.Iterables;
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import com.webank.wecube.plugins.alicloud.support.AcsClientStub;
-import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.RDSMariaDBSpecs;
-import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.RDSMySQLSpecs;
+import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.CoreMemorySpec;
+import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.RDSMariaDBSpec;
+import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.RDSMySQLSpec;
+import com.webank.wecube.plugins.alicloud.support.resourceSeeker.specs.SpecInfo;
 import org.apache.commons.lang3.EnumUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -50,30 +51,27 @@ public class RDSResourceSeeker {
         this.acsClientStub = acsClientStub;
     }
 
-    public String findAvailableResource(IAcsClient client, String engine, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
-        String foundResourceResult;
+    public SpecInfo findAvailableResource(IAcsClient client, String engine, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
+        SpecInfo result;
         switch (EnumUtils.getEnumIgnoreCase(RDSEngine.class, engine)) {
             case MYSQL:
-                foundResourceResult = findAvailableMySQLResource(client, coreMemoryString, regionId, zoneId, engineVersion, instanceChargeType, dBInstanceStorageType, category);
+                result = findAvailableMySQLResource(client, coreMemoryString, regionId, zoneId, engineVersion, instanceChargeType, dBInstanceStorageType, category);
                 break;
             case MARIADB:
-                foundResourceResult = findAvailableMariaDBResource(client, coreMemoryString, regionId, zoneId, engineVersion, instanceChargeType, dBInstanceStorageType, category);
+                result = findAvailableMariaDBResource(client, coreMemoryString, regionId, zoneId, engineVersion, instanceChargeType, dBInstanceStorageType, category);
                 break;
             default:
                 String msg = String.format("The plugin doesn't support the given RDS engine: [%s]", engine);
                 logger.error(msg);
                 throw new PluginException(msg);
         }
-        if (StringUtils.isEmpty(foundResourceResult)) {
-            throw new PluginException("Cannot find available resource.");
-        }
-        return foundResourceResult;
+
+        return result;
 
     }
 
-    private String findAvailableMySQLResource(IAcsClient client, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
+    private SpecInfo findAvailableMySQLResource(IAcsClient client, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
         DescribeAvailableResourceRequest request = new DescribeAvailableResourceRequest();
-        request.setRegionId(regionId);
         request.setZoneId(zoneId);
         request.setEngine(RDSEngine.MYSQL.getEngine());
         request.setEngineVersion(engineVersion);
@@ -81,15 +79,14 @@ public class RDSResourceSeeker {
         request.setDBInstanceStorageType(dBInstanceStorageType);
         request.setCategory(category);
 
-        final DescribeAvailableResourceResponse response = acsClientStub.request(client, request);
+        final DescribeAvailableResourceResponse response = acsClientStub.request(client, request, regionId);
 
         return matchResourceStringFromList(RDSEngine.MYSQL, coreMemoryString, response);
     }
 
 
-    private String findAvailableMariaDBResource(IAcsClient client, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
+    private SpecInfo findAvailableMariaDBResource(IAcsClient client, String coreMemoryString, String regionId, String zoneId, String engineVersion, String instanceChargeType, String dBInstanceStorageType, String category) {
         DescribeAvailableResourceRequest request = new DescribeAvailableResourceRequest();
-        request.setRegionId(regionId);
         request.setZoneId(zoneId);
         request.setEngine(RDSEngine.MARIADB.getEngine());
         request.setEngineVersion(engineVersion);
@@ -97,14 +94,13 @@ public class RDSResourceSeeker {
         request.setDBInstanceStorageType(dBInstanceStorageType);
         request.setCategory(category);
 
-        final DescribeAvailableResourceResponse response = acsClientStub.request(client, request);
+        final DescribeAvailableResourceResponse response = acsClientStub.request(client, request, regionId);
 
         return matchResourceStringFromList(RDSEngine.MARIADB, coreMemoryString, response);
     }
 
 
-    private String matchResourceStringFromList(RDSEngine engine, String coreMemoryString, DescribeAvailableResourceResponse response) throws PluginException {
-        String locCoreMemoryString = coreMemoryString.toUpperCase();
+    private SpecInfo matchResourceStringFromList(RDSEngine engine, String coreMemoryString, DescribeAvailableResourceResponse response) throws PluginException {
 
         List<DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedCategory.SupportedStorageType.AvailableResource> foundResourceList;
         try {
@@ -125,13 +121,13 @@ public class RDSResourceSeeker {
         }
 
         final List<String> dBInstanceClassStr = foundResourceList.stream().map(DescribeAvailableResourceResponse.AvailableZone.SupportedEngine.SupportedEngineVersion.SupportedCategory.SupportedStorageType.AvailableResource::getDBInstanceClass).collect(Collectors.toList());
-        List<String> matchResult = new ArrayList<>();
+        List<Map.Entry<String, CoreMemorySpec>> matchResult = new ArrayList<>();
         switch (engine) {
             case MYSQL:
-                matchResult = dBInstanceClassStr.stream().filter(s -> StringUtils.equals(locCoreMemoryString, RDSMySQLSpecs.matchByInstanceClass(s))).collect(Collectors.toList());
+                matchResult = RDSMySQLSpec.matchResource(dBInstanceClassStr, coreMemoryString);
                 break;
             case MARIADB:
-                matchResult = dBInstanceClassStr.stream().filter(s -> StringUtils.equals(locCoreMemoryString, RDSMariaDBSpecs.matchByInstanceClass(s))).collect(Collectors.toList());
+                matchResult = RDSMariaDBSpec.matchResource(dBInstanceClassStr, coreMemoryString);
                 break;
             default:
                 break;
@@ -141,7 +137,8 @@ public class RDSResourceSeeker {
             throw new PluginException("The given coreMemoryString doesn't match all available resources.");
         }
 
-        return Iterables.getLast(matchResult);
+        return new SpecInfo(matchResult.get(0).getKey(), matchResult.get(0).getValue());
+
     }
 
 
