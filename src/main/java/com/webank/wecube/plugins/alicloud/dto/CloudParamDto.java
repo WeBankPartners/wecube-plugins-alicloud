d441415a6a6dc5d92856b614661ad43789caf139
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-15 16:20:42 +0800

    #4 update CloudParamDto, when there is no field in the map, throw exception

--- src/main/java/com/webank/wecube/plugins/alicloud/dto/CloudParamDto.java
+++ src/main/java/com/webank/wecube/plugins/alicloud/dto/CloudParamDto.java
@@ -2,6 +2,7 @@ package com.webank.wecube.plugins.alicloud.dto;
 
 import com.webank.wecube.plugins.alicloud.common.PluginException;
 import com.webank.wecube.plugins.alicloud.utils.PluginMapUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -18,7 +19,11 @@ public class CloudParamDto {
 
     public static CloudParamDto convertFromString(String paramStr) throws PluginException {
         final Map<String, String> map = PluginMapUtils.fromCoreParamString(paramStr);
-        return new CloudParamDto(map.get("regionId"));
+        final String regionId = map.get("regionId");
+        if (StringUtils.isEmpty(regionId)) {
+            throw new PluginException("Cannot get field: [regionId] through the given cloudParams string");
+        }
+        return new CloudParamDto(regionId);
     }
 
     public String getRegionId() {
