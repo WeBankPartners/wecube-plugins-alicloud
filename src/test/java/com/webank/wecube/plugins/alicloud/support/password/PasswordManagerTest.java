646708e0cfa9104a27cb23dda1035a574c7546ea
tangjiawei <1135176001@qq.com>
2024-12-31 11:17:17 +0800

    Update PasswordManagerTest.java
    安全工单处理,去掉密码等敏感信息

--- src/test/java/com/webank/wecube/plugins/alicloud/support/password/PasswordManagerTest.java
+++ src/test/java/com/webank/wecube/plugins/alicloud/support/password/PasswordManagerTest.java
@@ -10,22 +10,14 @@ public class PasswordManagerTest extends BaseSpringBootTest {
     @Autowired
     private PasswordManager passwordManager;
 
-    private final String guid = "0015_0000000020";
-    private final String seed = "seed-wecube2.1-2020";
-    private final ***REMOVED***
-    private final String encryptedPassword = "{cipher_a}5f5fb6fd46cd7cf9183893425d1d6506";
-
-
     @Test
     public void givenGuidSeedAndPassword_encryptPassword_shouldSucceed() {
-        final String encryptedPassword = passwordManager.encryptPassword(guid, seed, this.password);
-        assert this.encryptedPassword.equals(encryptedPassword);
+      
     }
 
     @Test
     public void givenGuidSeedAndPassword_decryptPassword_shouldSucceed() {
-        final String decryptedPassword = passwordManager.decryptPassword(guid, seed, this.encryptedPassword);
-        assert this.password.equals(decryptedPassword);
+       
     }
 }
 
